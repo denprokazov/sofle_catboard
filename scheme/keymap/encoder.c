156d4bd7e5ac6bb854f5e76226ce6ca527956962
denprokazov <denprokazov@gmail.com>
2023-10-31 00:26:12 +0100

    Temporarely disabled encoder pageup/down

--- scheme/keymap/encoder.c
+++ scheme/keymap/encoder.c
@@ -9,12 +9,6 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         } else {
             tap_code(KC_VOLD);
         }
-    } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
     }
     return true;
 }
