f1af2b95b9f27e35bdf06c131be0d5bbbee1e817
denprokazov <denprokazov@gmail.com>
2023-11-10 21:16:35 +0100

    Added language switch on one button. Added menu button to layout. Fixed alt

--- scheme/keymap/config.h
+++ scheme/keymap/config.h
@@ -1,5 +1,5 @@
 #pragma once
 #define MASTER_LEFT
-
+#define DEBOUNCE 10
 #define SPLIT_WPM_ENABLE
 #define SPLIT_OLED_ENABLE
--- scheme/keymap/keymap.c
+++ scheme/keymap/keymap.c
@@ -19,7 +19,6 @@ enum custom_keycodes {
     KC_DLINE
 };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
@@ -41,8 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
   KC_LGUI,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  LSFT(KC_LALT),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RBRC,
-                KC_LGUI, KC_LALT, MO(_LOWER), KC_LCTL,  KC_LSFT,      KC_SPC, KC_RCTL, MO(_RAISE) , KC_MINUS, KC_MENU
+  KC_F13,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RBRC,
+                KC_LGUI, KC_LALT, MO(_LOWER), KC_LCTL,  KC_LSFT,      KC_SPC, KC_RCTL, MO(_RAISE) , KC_MINUS, KC_APP
 ),
 /*
  * COLEMAK
@@ -65,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
   KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LGUI,KC_LALT, MO(_LOWER), KC_LCTL,  KC_ENT,      KC_SPC, KC_RCTL, MO(_RAISE) , KC_MINUS, KC_MENU
+                 KC_LGUI, KC_LALT, MO(_LOWER), KC_LCTL,  KC_ENT,      KC_SPC, KC_RCTL, MO(_RAISE) , KC_MINUS, KC_MENU
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -134,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -259,6 +258,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
             break;
+        case KC_F13:
+            if (keymap_config.swap_lctl_lgui) {
+                if (record->event.pressed) { // Press Control space
+                    register_code(KC_LCTL);
+                    _delay_ms(50);
+                    register_code(KC_SPACE);
+                } else {
+                    unregister_code(KC_LCTL);
+                    _delay_ms(50);
+                    unregister_code(KC_SPACE);
+                }
+            } else { // Press Alt+Shift
+                if (record->event.pressed) { // Press Control space
+                    register_code(KC_LALT);
+                    _delay_ms(50);
+                    register_code(KC_LSFT);
+                } else {
+                    unregister_code(KC_LALT);
+                    _delay_ms(50);
+                    unregister_code(KC_LSFT);
+                }
+            }
+            return false;
+            break;
         case KC_UNDO:
             if (record->event.pressed) {
                 register_mods(mod_config(MOD_LCTL));
@@ -270,6 +293,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
     }
     return true;
-}
+};
 
 
